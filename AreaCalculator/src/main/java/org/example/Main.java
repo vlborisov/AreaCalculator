5a2af72d33ce310a32050ef94970a64018b5d512
Vladislav Borisov <93284707+yanestabilniypomogite@users.noreply.github.com>
2024-07-11 15:26:58 +0300

    Add files via upload

---
+++ AreaCalculator/src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Привет! Я могу посчитать площадь прямоугольника.\nВведите длину первой стороны(в см):");
+        int lenght = inputValue();
+        System.out.print("Введите длину второй стороны(в см):");
+        int width = inputValue();
+        int area = lenght * width;
+        System.out.printf("Площадь прямоугольника со сторонами %s и %s равна %s см^2%n", lenght, width, area);
+    }
+
+    public static int inputValue() {
+        while (true) {
+            int num = isDigit();
+            if (num > 0) {
+                return num;
+            } else {
+                System.out.print("Некорректный ввод!Введите положительное целое число: ");
+            }
+        }
+    }
+
+    public static int isDigit() {
+        while (true) {
+            try {
+                return new Scanner(System.in).nextInt();
+            } catch (Exception e) {
+                System.out.print("Некорректный ввод!Введите положительное целое число: ");
+            }
+        }
+    }
+}
